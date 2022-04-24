999cf19eb002b7ec5ea212ec20d79e6ab2778fd9
mauro-balades <mauro.balades@tutanota.com>
2022-04-24 13:40:24 +0200

    added a very basic cursor animation

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,8 @@
 
-class Blobify {
+// @ts-ignore
+import { default as Kinet } from 'kinet';
+
+export default class Blobify {
 
     public readonly defaultOptions = {
         rect: {
@@ -9,16 +12,63 @@ class Blobify {
         radius: 50
     };
 
+    private cursor: HTMLDivElement;
+    private kinet: Kinet;
+
+    private options: Object;
+
     constructor(options = {}) {
 
         // Merge default configuration
         Object.assign({}, this.defaultOptions, options)
+        this.options = options;
+
+        // Create a new HTML element and append it to the document's body
+        this.cursor = document.createElement("div");
+        document.body.appendChild(this.cursor);
+
+        // Style the cursor
+        this.circle_default(options)
+
+        this.kinet = new Kinet({
+            acceleration: 0.06,
+            friction: 0.20,
+            names: ["x", "y"],
+        });
+
+        // set handler on kinet tick event
+        this.kinet.on('tick', this.kinet_tick.bind(this));
+
+        // call kinet animate method on mousemove
+        document.addEventListener('mousemove', this.mousemove.bind(this));
     }
 
-    private createCursor() {
+    // Events
 
+    private mousemove(event: MouseEvent) {
+        this.kinet.animate('x', event.clientX - window.innerWidth/2);
+        this.kinet.animate('y', event.clientY - window.innerHeight/2);
     }
 
+    // Kinet events
+    private kinet_tick(instances: any) {
+        this.cursor.style.transform = `translate3d(${ (instances.x.current) }px, ${ (instances.y.current) }px, 0) rotateX(${ (instances.x.velocity/2) }deg) rotateY(${ (instances.y.velocity/2) }deg)`;
+    }
+
+    // styles
+    private circle_default(options: any) {
+        this.cursor.style.width = '40px';
+        this.cursor.style.height = '40px';
+        this.cursor.style.background = 'linear-gradient(to top left, #0062bE, #00A2FE)';
+        this.cursor.style.borderRadius = '50%';
+        this.cursor.style.position = 'absolute';
+        this.cursor.style.top = '50%';
+        this.cursor.style.left = '50%';
+        this.cursor.style.margin = '-20px 0 0 -20px';
+        this.cursor.style.left = '50%';
+        this.cursor.style.pointerEvents = 'none';
+        this.cursor.style.mixBlendMode = 'multiply';
+        this.cursor.style.zIndex = '10';
+    }
 }
 
-export default Blobify;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './lib/index.ts',
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.ts?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: {
+            type: "umd",  // see https://webpack.js.org/configuration/output/#outputlibrarytype
+            export: "default",  // see https://github.com/webpack/webpack/issues/8480
+            name: 'Blobify',
+        }
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+};
