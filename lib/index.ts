945161d1ee9c8aea19e39943edd21d7715f4a96a
mauro-balades <mauro.balades@tutanota.com>
2022-04-24 11:03:26 +0200

    added some functions to blobify class

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,24 @@
 
 class Blobify {
 
-    public readonly defaultOptions = {};
+    public readonly defaultOptions = {
+        rect: {
+            width: 20,
+            height: 20
+        },
+        radius: 50
+    };
 
     constructor(options = {}) {
 
         // Merge default configuration
         Object.assign({}, this.defaultOptions, options)
     }
+
+    private createCursor() {
+
+    }
+
 }
 
 export default Blobify;
