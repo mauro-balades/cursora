0e9890bdc57a51d0b6106ee58e81190b1b612618
mauro-balades <mauro.balades@tutanota.com>
2022-04-24 08:45:55 +0200

    merging default options with the provided options

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,13 @@
 
 class Blobify {
-    
+
+    public readonly defaultOptions = {};
+
+    constructor(options = {}) {
+
+        // Merge default configuration
+        Object.assign({}, this.defaultOptions, options)
+    }
 }
 
 export default Blobify;
